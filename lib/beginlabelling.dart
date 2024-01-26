57b84054e94e2ad470a983235c0596f666229129
Pi <paingthetko.pi@gmail.com>
2024-01-26 08:09:26 +0400

    Labelling part done

--- lib/beginlabelling.dart
+++ lib/beginlabelling.dart
@@ -44,46 +44,46 @@ class _BeginLabellingPageState extends State<BeginLabellingPage> {
     }
     return [];
   }
-Future<List<File>> _loadImageFiles() async {
-  // Retrieve the saved image folder path
-  final folderPathFile = await _getLocalFile('folder_path.txt');
-  if (!await folderPathFile.exists()) {
-    _showAlert('Folder path is not set.');
-    return [];
+
+  Future<List<File>> _loadImageFiles() async {
+    // Retrieve the saved image folder path
+    final folderPathFile = await _getLocalFile('folder_path.txt');
+    if (!await folderPathFile.exists()) {
+      _showAlert('Folder path is not set.');
+      return [];
+    }
+
+    String folderPath = await folderPathFile.readAsString();
+    final imageDirectory = Directory(folderPath);
+
+    if (!await imageDirectory.exists()) {
+      _showAlert('Selected folder does not exist.');
+      return [];
+    }
+
+    return imageDirectory
+        .listSync()
+        .whereType<File>()
+        .where((file) => _isImageFile(file.path))
+        .toList();
   }
 
-  String folderPath = await folderPathFile.readAsString();
-  final imageDirectory = Directory(folderPath);
-
-  if (!await imageDirectory.exists()) {
-    _showAlert('Selected folder does not exist.');
-    return [];
+  Future<File> _getLocalFile(String fileName) async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/$fileName');
   }
 
-  return imageDirectory
-      .listSync()
-      .whereType<File>()
-      .where((file) => _isImageFile(file.path))
-      .toList();
-}
-
-Future<File> _getLocalFile(String fileName) async {
-  final directory = await getApplicationDocumentsDirectory();
-  return File('${directory.path}/$fileName');
-}
-
-void _showAlert(String message) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Error'),
-        content: Text(message),
-      );
-    },
-  );
-}
-
+  void _showAlert(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+        );
+      },
+    );
+  }
 
   bool _isImageFile(String filePath) {
     var extension = filePath.split('.').last.toLowerCase();
@@ -111,71 +111,95 @@ void _showAlert(String message) {
   }
 
   Future<void> _saveLabelsToFile() async {
-    // Save the selected labels to the corresponding .txt file
+    String txtFilePath = _imageFiles[_currentImageIndex]
+        .path
+        .replaceAll(RegExp(r'\.[^\.]+$'), '.txt');
+    File txtFile = File(txtFilePath);
+    await txtFile.writeAsString(_selectedLabels.join('\n'));
   }
 
   Future<void> _goToNextImage() async {
-  if (_currentImageIndex < _imageFiles.length - 1) {
-    _currentImageIndex++;
-    await _prefs.setInt('lastLabelledImageIndex', _currentImageIndex);
-    _selectedLabels = await _loadSelectedLabels(_imageFiles[_currentImageIndex]);
-    setState(() {});
+    if (_currentImageIndex < _imageFiles.length - 1) {
+      _currentImageIndex++;
+      await _prefs.setInt('lastLabelledImageIndex', _currentImageIndex);
+      _selectedLabels =
+          await _loadSelectedLabels(_imageFiles[_currentImageIndex]);
+      setState(() {});
+    }
   }
-}
-
 
   Future<void> _goToPreviousImage() async {
-  if (_currentImageIndex > 0) {
-    _currentImageIndex--;
-    await _prefs.setInt('lastLabelledImageIndex', _currentImageIndex);
-    _selectedLabels = await _loadSelectedLabels(_imageFiles[_currentImageIndex]);
-    setState(() {});
+    if (_currentImageIndex > 0) {
+      _currentImageIndex--;
+      await _prefs.setInt('lastLabelledImageIndex', _currentImageIndex);
+      _selectedLabels =
+          await _loadSelectedLabels(_imageFiles[_currentImageIndex]);
+      setState(() {});
+    }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Begin Labelling'),
+        title: const Text('Labelling Area'),
       ),
       body: _imageFiles == null || _imageFiles.isEmpty
           ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Card(
-                  child: Image.file(_imageFiles[_currentImageIndex]),
-                ),
-                Wrap(
-                  children: _labels.map((label) {
-                    bool isSelected = _selectedLabels.contains(label);
-                    return ChoiceChip(
-                      label: Text(label),
-                      selected: isSelected,
-                      onSelected: (bool selected) {
-                        _toggleLabel(label);
-                      },
-                    );
-                  }).toList(),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: _goToPreviousImage,
-                      child: const Text('Previous Image'),
+          : SingleChildScrollView(
+              // Added to ensure the page is scrollable
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LinearProgressIndicator(
+                      value: _currentImageIndex / (_imageFiles.length - 1),
                     ),
-                    ElevatedButton(
-                      onPressed: _goToNextImage,
-                      child: const Text('Next Image'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Wrap(
+                      spacing: 8.0, // Space between chips
+                      children: _labels.map((label) {
+                        bool isSelected = _selectedLabels.contains(label);
+                        return ChoiceChip(
+                          label: Text(label),
+                          selected: isSelected,
+                          onSelected: (bool selected) {
+                            _toggleLabel(label);
+                          },
+                        );
+                      }).toList(),
                     ),
-                  ],
-                ),
-                LinearProgressIndicator(
-                  value: _currentImageIndex / (_imageFiles.length - 1),
-                ),
-              ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: _goToPreviousImage,
+                          child: const Text('Previous Image'),
+                        ),
+                        ElevatedButton(
+                          onPressed: _goToNextImage,
+                          child: const Text('Next Image'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: AspectRatio(
+                      aspectRatio: 16 / 9, // Set your desired aspect ratio
+                      child: Card(
+                        child: Image.file(_imageFiles[_currentImageIndex],
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
     );
   }
